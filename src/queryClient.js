ef5211967edda367d1a42fbaa8c4d1fdc8309b3a
rakeshpatel17 <rakeshpatellingam@gmail.com>
2025-07-15 15:00:24 +0530

    commented caching

--- src/queryClient.js
+++ src/queryClient.js
@@ -1,14 +1,14 @@
-import { QueryClient } from '@tanstack/react-query';
+// import { QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,    // 5m
-      cacheTime: 1000 * 60 * 60,   // 1h
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-      retry: 1,
-      retryDelay: 1000,
-    },
-  },
-});
+// export const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       staleTime: 1000 * 60 * 5,    // 5m
+//       cacheTime: 1000 * 60 * 60,   // 1h
+//       refetchOnMount: false,
+//       refetchOnWindowFocus: false,
+//       retry: 1,
+//       retryDelay: 1000,
+//     },
+//   },
+// });
